9b605212e0ed4bd2cc57cf3b7a5c1f7e1178e592
Longs Pemun Gotar <developerlongs@gmail.com>
2022-09-11 11:43:41 +0000

    initial commit

--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -2,11 +2,21 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import "src/Contract.sol";
 
-contract ContractTest is Test {
+contract ContractTest is Test, Contract {
     function setUp() public {}
 
     function testExample() public {
         assertTrue(true);
+    }
+
+    function testCount() public {
+        assertEq(count, 0); 
+    }
+
+     function testPLus() public {
+        plus();
+        assertEq(count, 1); 
     }
 }
